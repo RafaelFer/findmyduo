3f8a57a0b67f722d56765d816d86b94f6c082a0d
Rafael Ferreira <rafael22fs@gmail.com>
2018-10-16 18:44:25 -0300

    estacoes

--- findmyduo/src/main/java/br/com/findmyduo/controller/HomeController.java
+++ findmyduo/src/main/java/br/com/findmyduo/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import sun.misc.Contended;
 import sun.plugin.javascript.navig.LinkArray;
 
@@ -31,34 +32,13 @@ public class HomeController {
 
 
     @RequestMapping("/")
-    public String init(Model model){
-        //Iterable<Linha> linhas = repository.findAll();
-        //model.addAttribute("linhas",linhas);
+    public String init(Model model) {
 
-        Estacao estacao1 = new Estacao();
-        estacao1.setNome("Republica");
-
-        Estacao estacao2 = new Estacao();
-        estacao2.setNome("Sé");
-
-        Linha linha = new Linha();
-        linha.setNome("VERMELHA");
-        linha.setTipoLinhaEnum(TipoLinhaEnum.METRO);
-
-        linha.setEstacoes(Arrays.asList(estacao1,estacao2));
-
-
-        estacaoRepository.save(estacao1);
-        estacaoRepository.save(estacao2);
-        repository.save(linha);
-
+        List<Estacao> estacoes = (List<Estacao>) estacaoRepository.findAll();
         List<Linha> linhas = (List<Linha>) repository.findAll();
 
-        for (Linha linha1 : linhas) {
-            for (Estacao estacao : linha.getEstacoes()) {
-                System.out.println(linha.getNome()+" - "+estacao.getNome());
-            }
-        }
+        model.addAttribute("linhas", linhas);
+        model.addAttribute("estacoes", estacoes);
 
         return "index";
     }
