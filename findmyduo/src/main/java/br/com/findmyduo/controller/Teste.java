cc3e5766e2e3c4378806ca305c3e34f036af6f24
Rafael Ferreira <rafael22fs@gmail.com>
2018-10-31 01:19:05 -0300

    Commitando classe de teste via intellij

---
+++ findmyduo/src/main/java/br/com/findmyduo/controller/Teste.java
@@ -0,0 +1,11 @@
+package br.com.findmyduo.controller;
+
+/**
+ * Created by user on 31/10/2018.
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("banch teste");
+    }
+}
